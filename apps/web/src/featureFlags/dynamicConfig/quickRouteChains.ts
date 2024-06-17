51e3c488d87f4959326f9586ebff5e32bfac2b84
darshitbhalodialampros <darshitbhalodia@lampros.tech>
2024-06-17 23:08:52 +0530

    mode chain

--- apps/web/src/featureFlags/dynamicConfig/quickRouteChains.ts
+++ apps/web/src/featureFlags/dynamicConfig/quickRouteChains.ts
@@ -7,7 +7,7 @@ export const QUICK_ROUTE_CONFIG_KEY = "quick_route_chains";
 export function useQuickRouteChains(): ChainId[] {
   const statsigConfig = useDynamicConfig(DynamicConfigs.QuickRouteChains);
   const chains = statsigConfig.get(QUICK_ROUTE_CONFIG_KEY, []) as ChainId[];
-  if (chains.every((c) => Object.values(ChainId).includes(c))) {
+  if (ChainId.MODE) {
     return chains;
   } else {
     console.error("dynamic config chains contain invalid ChainId");
